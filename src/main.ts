fafb1c34d730de020e4f2896a0d7f0f6f6b1c0af
Kutsak-Nataly <73638937+Kutsak-Nataly@users.noreply.github.com>
2021-07-04 17:36:20 +0300

    feat: add fastyfy

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './error_handler/http-exception.filter';
 import { loggerMiddleware } from './error_handler/logger.middleware';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+const fastyfy = process.env.USE_FASTIFY;
+const PORT = process.env.PORT;
+const TYPEORM_HOST = process.env.TYPEORM_HOST;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter());
-  const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
-  app.use(loggerMiddleware);
-  await app.listen(port);
+  if (fastyfy) {
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter()
+    );
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.use(loggerMiddleware);
+    await app.listen(PORT, TYPEORM_HOST);
+    console.log('Run fastyfy');
+  } else {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.use(loggerMiddleware);
+    await app.listen(PORT, TYPEORM_HOST);
+  }
 }
 
-bootstrap().then();
+bootstrap()
+    .then(() => {
+      console.log(`App is running on http://${TYPEORM_HOST}:${PORT}`)
+    })
+    .catch((e) => {
+      console.log('Failed to run App', e.message);
+    });
